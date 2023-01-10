dbfca0bd1146482e20a970bc2f31b1cfb3a7f1a7
Reda-Ghost <86271659+Reda-Ghost@users.noreply.github.com>
2023-01-10 11:53:36 +0100

    add mobile menu & fix some bugs & add header animation

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,3 +1,4 @@
+// ========= SVG ANIMATION =============
 // svg elements of the circle
 const item1 = document.getElementById('item_1');
 const item2 = document.getElementById('item_2');
@@ -16,29 +17,65 @@ function animateColor() {
 		item2.style.fill = palletColors['light-green'];
 		item3.style.fill = palletColors['light-pink'];
 		item4.style.fill = palletColors['light-grey'];
-	}, 1000);
+	}, 800);
 	setTimeout(() => {
 		item1.style.fill = palletColors['light-green'];
 		item2.style.fill = palletColors['light-pink'];
 		item3.style.fill = palletColors['light-grey'];
-	}, 2000);
+	}, 1600);
 	setTimeout(() => {
 		item5.style.fill = palletColors['light-green'];
 		item1.style.fill = palletColors['light-pink'];
 		item2.style.fill = palletColors['light-grey'];
-	}, 3000);
+	}, 2400);
 	setTimeout(() => {
 		item4.style.fill = palletColors['light-green'];
 		item5.style.fill = palletColors['light-pink'];
 		item1.style.fill = palletColors['light-grey'];
-	}, 4000);
+	}, 3200);
 	setTimeout(() => {
 		item3.style.fill = palletColors['light-green'];
 		item4.style.fill = palletColors['light-pink'];
 		item5.style.fill = palletColors['light-grey'];
-	}, 5000);
+	}, 4000);
 }
 
 animateColor(); // to start the animation immediately after the page loaded
 
-setInterval(animateColor, 7000); // then do the animation every 7s
+setInterval(animateColor, 6000); // then do the animation every 7s
+
+// ======== SHOW / HIDE MOBILE MENU ============
+const showMenu = (toggleId, navId) => {
+	const toggle = document.getElementById(toggleId),
+		nav = document.getElementById(navId);
+
+	// Validate that variables exist
+	if (toggle && nav) {
+		toggle.addEventListener('click', () => {
+			nav.classList.toggle('show-menu');
+		});
+	}
+};
+showMenu('nav-toggle', 'nav-menu');
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+	const navMenu = document.getElementById('nav-menu');
+	// When we click on each nav__link, we remove the show-menu class
+	navMenu.classList.remove('show-menu');
+}
+navLinks.forEach((link) => link.addEventListener('click', linkAction));
+
+// ========== FIXED AND ADD SHADOW TO HEADER ON SCROLL =============
+
+function scrollHeader() {
+	const nav = document.getElementById('header');
+	// When the scroll is greater than 80 viewport height, make the box shadow to the header element
+	window.addEventListener('scroll', () => {
+		this.scrollY > 80
+			? nav.classList.add('scroll-header')
+			: nav.classList.remove('scroll-header');
+	})
+}
+window.addEventListener('scroll', scrollHeader);
